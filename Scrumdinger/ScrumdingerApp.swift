9ec457677ea8c62ae72bb9ae6cf032ec967c61ba
Arunesh Mishra <aruneshm@gmail.com>
2023-02-15 21:59:59 -0700

    Initial Commit

---
+++ Scrumdinger/ScrumdingerApp.swift
@@ -0,0 +1,17 @@
+//
+//  ScrumdingerApp.swift
+//  Scrumdinger
+//
+//  Created by Arunesh Mishra on 2/15/23.
+//
+
+import SwiftUI
+
+@main
+struct ScrumdingerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
